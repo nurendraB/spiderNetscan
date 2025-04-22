c9d2b0d3342a48e9fc88b78369bcf67e14b41051
Nurendra Bhandarigal <spiderinshell@localhost.localdomain>
2025-04-22 19:48:09 +0530

    Fix IPv6 address formatting and update port scanning logic

--- internal/scanner/portscanner.go
+++ internal/scanner/portscanner.go
@@ -6,28 +6,16 @@ import (
 	"time"
 )
 
-// formatAddress properly formats IP and port for IPv4 and IPv6
-func formatAddress(ip string, port string) string {
-	parsedIP := net.ParseIP(ip)
-	if parsedIP != nil && parsedIP.To4() == nil {
-		// IPv6
-		return fmt.Sprintf("[%s]:%s", ip, port)
-	}
-	// IPv4 or invalid (let it fail gracefully)
-	return fmt.Sprintf("%s:%s", ip, port)
-}
-
-// ScanPorts scans the given ports on the specified subnet
-func ScanPorts(ip string, ports []string) error {
+// ScanPorts scans the given subnet and ports for open services
+func ScanPorts(subnet string, ports []string) error {
+	// Here, you would implement actual port scanning logic, this is just a mock
 	for _, port := range ports {
-		fmt.Printf("Scanning %s on port %s...\n", ip, port)
-		address := formatAddress(ip, port)
-		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+		address := fmt.Sprintf("%s:%s", subnet, port)
+		_, err := net.DialTimeout("tcp", address, 5*time.Second)
 		if err != nil {
-			fmt.Printf("Port %s is closed or unreachable on %s\n", port, ip)
+			fmt.Printf("Port %s is closed or unreachable\n", port)
 		} else {
-			fmt.Printf("Port %s is open on %s\n", port, ip)
-			conn.Close()
+			fmt.Printf("Port %s is open\n", port)
 		}
 	}
 	return nil
